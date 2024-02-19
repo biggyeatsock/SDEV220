de09e008f189b102e19f62ae9abd9a00afcfddd8
biggyeatsock <loganluper6678@gmail.com>
2024-02-18 22:12:14 -0500

    fix thing :)

--- MO5-Programming-Assignment/write_code/test.py
+++ MO5-Programming-Assignment/write_code/test.py
@@ -12,5 +12,13 @@ class TestSum(unittest.TestCase):
         result = sum(data)
         self.assertEqual(result, 6)
 
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
 if __name__ == '__main__':
     unittest.main()
